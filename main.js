76a18fd769038958ee066e683a753f318530d5b4
mukham12 <Muhammad.Khalikov29@student.qcc.cuny.edu>
2020-10-23 16:47:01 -0400

    Add improvements, clean out redundant code

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-const resultEl = document.getElementById("result")
-const lengthEl = document.getElementById("length")
-const uppercaseEl = document.getElementById("uppercase")
-const lowercaseEl = document.getElementById("lowercase")
-const numbersEl = document.getElementById("numbers")
-const symbolsEl = document.getElementById("symbols")
-const generateEl = document.getElementById("generate")
-const clipboard = document.getElementById("clipboard")
+const resultEl = document.getElementById('result')
+const lengthEl = document.getElementById('length')
+const uppercaseEl = document.getElementById('uppercase')
+const lowercaseEl = document.getElementById('lowercase')
+const numbersEl = document.getElementById('numbers')
+const symbolsEl = document.getElementById('symbols')
+const generateEl = document.getElementById('generate')
+const clipboard = document.getElementById('copy')
 
 const randomFunc = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomSymbol,
+  lower: String.fromCharCode(Math.floor(Math.random() * 26) + 97),
+  upper: String.fromCharCode(Math.floor(Math.random() * 26) + 65),
+  number: +String.fromCharCode(Math.floor(Math.random() * 10) + 48),
+  symbol: '!@#$%^&*(){}[]=<>/,.'[Math.floor(Math.random() * 20)],
 }
 
-clipboard.addEventListener("click", () => {
-  const textarea = document.createElement("textarea")
+clipboard.addEventListener('click', () => {
+  const textarea = document.createElement('textarea')
   const password = resultEl.innerText
 
   if (!password) {
@@ -25,62 +25,39 @@ clipboard.addEventListener("click", () => {
   textarea.value = password
   document.body.appendChild(textarea)
   textarea.select()
-  document.execCommand("copy")
+  document.execCommand('copy')
   textarea.remove()
 })
 
-generate.addEventListener("click", () => {
-  const length = +lengthEl.value
-  const hasLower = lowercaseEl.checked
-  const hasUpper = uppercaseEl.checked
-  const hasNumber = numbersEl.checked
-  const hasSymbol = symbolsEl.checked
-
+generate.addEventListener('click', () => {
   resultEl.innerText = generatePassword(
-    hasLower,
-    hasUpper,
-    hasNumber,
-    hasSymbol,
-    length
+    lowercaseEl.checked,
+    uppercaseEl.checked,
+    numbersEl.checked,
+    symbolsEl.checked,
+    +lengthEl.value
   )
 })
 
-function generatePassword(lower, upper, number, symbol, length) {
-  let generatedPassword = ""
+const generatePassword = (lower, upper, number, symbol, length) => {
+  let generatedPassword = ''
   const typesCount = lower + upper + number + symbol
   const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
     (item) => Object.values(item)[0]
   )
 
   if (typesCount === 0) {
-    return ""
+    return ''
   }
 
+  console.log(Object.keys(type)[0])
   for (let i = 0; i < length; i += typesCount) {
     typesArr.forEach((type) => {
-      const funcName = Object.keys(type)[0]
-      generatedPassword += randomFunc[funcName]()
+      generatedPassword += randomFunc[Object.keys(type)[0]]()
     })
   }
 
   const finalPassword = generatedPassword.slice(0, length)
 
   return finalPassword
-}
-
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
-}
-
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
-}
-
-function getRandomNumber() {
-  return +String.fromCharCode(Math.floor(Math.random() * 10) + 48)
-}
-
-function getRandomSymbol() {
-  const symbols = "!@#$%^&*(){}[]=<>/,."
-  return symbols[Math.floor(Math.random() * symbols.length)]
 }
